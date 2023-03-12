8490e09c0ef28a08f519f355b278dd6c52f3894e
Mohamed Achaq <motriyanox@gmail.com>
2023-03-13 00:20:54 +0100

    Fix og generator

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -24,7 +24,7 @@ export default async function handler(req: NextRequest) {
           flexDirection: "column",
           alignItems: "flex-start",
           justifyContent: "center",
-          backgroundImage: "url(http://localhost:3000/og-bg.png)",
+          backgroundImage: "url(https://achaq.dev/og-bg.png)",
         }}
       >
         <div
