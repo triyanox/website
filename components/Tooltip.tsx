22508cf5b2bedecdf97488c6cb1e9de0b3eba39f
Mohamed Achaq <motriyanox@gmail.com>
2023-03-14 10:32:52 +0100

    Add exit animation for the tooltip component 🖌️

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { motion } from "framer-motion";
-import { Fragment, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 type ToolTipProps = {
   children: React.ReactNode;
@@ -18,18 +18,19 @@ const ToolTip = ({ children, text, offset }: ToolTipProps) => {
       onMouseLeave={() => setShow(false)}
     >
       {children}
-      {show && (
-        <Fragment>
+      <AnimatePresence mode="wait">
+        {show && (
           <motion.div
             initial={{ opacity: 0, y: 20, rotateX: 180 }}
             animate={{ opacity: 1, y: 0, rotateX: 0 }}
+            exit={{ opacity: 0, y: 10, rotateX: 40 }}
             transition={{ type: "spring", stiffness: 500, damping: 30 }}
             className={`absolute bg-bg-primary-light dark:bg-bg-primary-dark px-6 py-2 rounded-2xl shadow-shadow-secondary dark:shadow-shadow-secondary-dark -top-10`}
           >
             {text}
           </motion.div>
-        </Fragment>
-      )}
+        )}
+      </AnimatePresence>
     </div>
   );
 };
